914f81b8e18143340ede331a497b20e01fd3ab9a
Samara Bastos <samarasampaio30@gmail.com>
2022-06-15 21:59:49 -0300

    Requisito 12

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,20 +1,24 @@
 // Desafio 11
 function generatePhoneNumber() {
-  // seu código aqui
 }
 
-// Desafio 12
-function triangleCheck() {
-  // seu código aqui
-}
+  // Desafio 12
+  function triangleCheck(lineA, lineB, lineC) {
+    if(lineA < Math.abs(lineB + lineC) && lineB < Math.abs(lineA + lineC) && lineC < Math.abs(lineA + lineB)){
+   return true;
+   } 
+   return false;
+  }
 
-// Desafio 13
-function hydrate() {
-  // seu código aqui
-}
 
-module.exports = {
-  generatePhoneNumber,
-  hydrate,
-  triangleCheck,
-};
+  // Desafio 13
+  function hydrate() {
+    // seu código aqui
+  }
+
+  module.exports = {
+    generatePhoneNumber,
+    hydrate,
+    triangleCheck,
+  }
+
